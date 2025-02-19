c6b75b9d4e1269bd1da9dc21f6de9484357cea1c
elly <mwananjelaelisha36@gmail.com>
2025-02-19 11:55:46 +0300

    Merged remote changes into local repository

---
+++ authentication/authentication/authapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Service, Form, Setie, Profile,Member
+
+# Register the models
+admin.site.register(Service)
+admin.site.register(Form)
+admin.site.register(Setie)
+admin.site.register(Profile)
+admin.site.register(Member)
